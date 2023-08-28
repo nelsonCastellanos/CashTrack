b589e3b930befc345ae548122854d79fd0116e40
Nelson Castellanos <nelson.castellanos@mercadolibre.com.co>
2023-08-27 20:50:24 -0500

    feat: add cors controll

--- next.config.js
+++ next.config.js
@@ -4,6 +4,20 @@ const nextConfig = {
     swcMinify: true,
     output: 'standalone',
     compress: false,
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: process.env.ACCESS_CONTROL_ALLOW_CREDENTIALS },
+                    { key: "Access-Control-Allow-Origin", value: process.env.ACCESS_CONTROL_ALLOW_ORIGIN },
+                    { key: "Access-Control-Allow-Methods", value: process.env.ACCESS_CONTROL_ALLOW_CREDENTIALS },
+                    { key: "Access-Control-Allow-Headers", value: process.env.ACCESS_CONTROL_ALLOW_CREDENTIALS },
+                ]
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
